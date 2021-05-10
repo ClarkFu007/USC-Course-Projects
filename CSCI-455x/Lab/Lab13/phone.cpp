ee09c804a5058511401847ff2a22579006000099
Yao(Clark) Fu <57007542+ClarkFu007@users.noreply.github.com>
2021-05-10 10:13:31 -0700

    Add files via upload

---
+++ CSCI-455x/Lab/Lab13/phone.cpp
@@ -0,0 +1,71 @@
+/*
+
+  CSCI 455 C String lab
+
+  See lab description for what it should do.  
+  C++ tools allowed: cout, call by reference, ostream output, new / delete
+
+*/
+
+
+// for C input functions (e.g., fgets used here)
+#include <cstdio>
+
+// C string functions
+#include <cstring>
+
+#include <iostream>
+
+
+using namespace std;
+
+const char NAME_DELIM = ':';
+const int AREA_CODE_SIZE = 3;
+const int PREFIX_SIZE = 3;
+const int LINE_NO_SIZE = 4;
+const int MAX_LINE_SIZE = 1024;  // including newline and terminating null char
+
+
+
+int main() {
+
+   char buffer[MAX_LINE_SIZE];
+
+
+   // fgets reads a line of input and puts it in a C string.  If the line of input
+   // is longer than the buffer array only gets enough chars that fit (and ignores the
+   // rest); this prevents it from overwriting the end of the array.
+   // Unlike Java Scanner nextLine(), fgets also saves the newline in the buffer.
+   // So after call, buffer will have 0 or more chars read from the line, 
+   // then a newline char ('\n'), and then the null char ('\0')
+   // note: the sizeof function below does not work with dynamic arrays.
+   // fgets returns 0 (false) when it hits EOF
+   // Note: stdin (third paremeter below) is the C version of cin or System.in
+
+
+   while (fgets(buffer, sizeof(buffer), stdin)) { 
+   //xxx-xxx-xxxx 
+   cout << "--------------------" << endl; 
+   cout << "LINE READ: " << buffer; 
+   char *areacodePT = new char[AREA_CODE_SIZE + 1]; 
+   char *prefixPT = new char[PREFIX_SIZE + 1]; 
+   char *linenoPT = new char[LINE_NO_SIZE + 1]; 
+   memcpy(areacodePT, buffer, AREA_CODE_SIZE); 
+   areacodePT[3] = '\0'; 
+   memcpy(prefixPT, buffer + 4, PREFIX_SIZE); 
+   prefixPT[3] = '\0'; 
+   memcpy(linenoPT, buffer + 8, LINE_NO_SIZE); 
+   linenoPT[4] = '\0'; 
+   cout << "area code " << areacodePT << endl; 
+   cout << "prefix code " << prefixPT << endl; 
+   cout << "line Number " << linenoPT << endl;
+      
+   delete areacodePT;
+   delete prefixPT;
+   delete linenoPT;
+      
+   } 
+  
+
+
+}
